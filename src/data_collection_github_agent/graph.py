318c635357c5d72dc721c64b2e1493fe92d46397
zhouxiaochi <jimmyzhou.ntu@gmail.com>
2025-04-13 18:24:33 +0100

    feature: updated graph.py
    - format
    - page number to 11

--- src/data_collection_github_agent/graph.py
+++ src/data_collection_github_agent/graph.py
@@ -1,7 +1,7 @@
 # graph.py
 import asyncio
-from contextlib import asynccontextmanager
 import os
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
@@ -9,26 +9,31 @@ from langchain_openai import ChatOpenAI
 from utils.load_mcp_config import load_mcp_config
 from src.data_collection_github_agent.prompt import DATA_COLLECTION_PROMPT
 
-# use dot env to get the api key and base url (from .env file), override the default ones
+# Load environment variables from .env file
 load_dotenv()
 mcp_config = load_mcp_config()
-model = ChatOpenAI(model="gpt-4o-mini", 
-                   base_url=os.getenv("BASE_URL"), 
-                   api_key=os.getenv("API_KEY"))
+model = ChatOpenAI(
+    model="gpt-4o-mini",
+    base_url=os.getenv("BASE_URL"),
+    api_key=os.getenv("API_KEY")
+)
 
 @asynccontextmanager
 async def make_graph():
-    async with MultiServerMCPClient(
-        mcp_config
-    ) as client:
+    async with MultiServerMCPClient(mcp_config) as client:
         agent = create_react_agent(model, client.get_tools())
         yield agent
 
 async def main():
     async with make_graph() as agent:
-        for page_number in range(1, 3):
+        for page_number in range(1, 11):
             print(f"Collecting files from page {page_number}")
-            result = await agent.ainvoke({"messages": DATA_COLLECTION_PROMPT.format(per_page=20, page_number=page_number)})
+            result = await agent.ainvoke({
+                "messages": DATA_COLLECTION_PROMPT.format(
+                    per_page=20,
+                    page_number=page_number
+                )
+            })
             print(result["messages"][-1].content)
 
 if __name__ == "__main__":
