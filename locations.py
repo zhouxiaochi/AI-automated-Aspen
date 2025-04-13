a927219f10630ee2f4740b6c5deec241cd1b4446
zhouxiaochi <jimmyzhou.ntu@gmail.com>
2025-04-13 18:26:23 +0100

    feature: make /data dir if not exist

--- locations.py
+++ locations.py
@@ -3,6 +3,10 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+# create data directory if it doesn't exist
+os.makedirs(DATA_DIR, exist_ok=True)
+
 CONFIG_DIR = os.path.join(ROOT_DIR, "configs")
 
 
